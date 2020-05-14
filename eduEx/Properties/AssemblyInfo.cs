608c33b37445f873a1de260884827bafa518d04e
Simon Rozman <simon@rozman.si>
2020-05-14 14:43:20 +0200

    Add assembly info
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ eduEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿/*
+    eduEx - Extensions for .NET
+
+    Copyright: 2020 The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Extensions for .NET")]
+[assembly: AssemblyCompany("SURF")]
+[assembly: AssemblyProduct("eduEx")]
+[assembly: AssemblyCopyright("Copyright © 2020 The Commons Conservancy eduVPN Programme")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e2c74ac-c81c-4cc6-b88d-81192623a66d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.0")]
