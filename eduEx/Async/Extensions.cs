7b356f25e74122f4eee2cf27dbb2ea1fc779a304
Simon Rozman <simon@rozman.si>
2022-02-01 13:04:52 +0100

    Copyright: Update
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduEx/Async/Extensions.cs
+++ eduEx/Async/Extensions.cs
@@ -1,7 +1,7 @@
 ﻿/*
     eduEx - Extensions for .NET
 
-    Copyright: 2021-2022 The Commons Conservancy eduVPN Programme
+    Copyright: 2021-2022 The Commons Conservancy
     SPDX-License-Identifier: GPL-3.0+
 */
 
